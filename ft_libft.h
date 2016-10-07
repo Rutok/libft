3703c04ce849e3ba158d1300f2d70772dbc7b842
Nicolas Boste <n.boste@gmail.com>
2016-10-07 17:45:39 +0200

    strchr

--- ft_libft.h
+++ ft_libft.h
@@ -6,7 +6,7 @@
 /*   By: nboste <nboste@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/06 21:21:09 by nboste            #+#    #+#             */
-/*   Updated: 2016/10/07 17:18:34 by nboste           ###   ########.fr       */
+/*   Updated: 2016/10/07 17:41:47 by nboste           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,5 +42,7 @@ char    *ft_strcat(char *dest, const char *src);
 char    *ft_strncat(char *dest, const char *src, size_t n);
 
 size_t  ft_strlcat(char *dest, const char *src, size_t size);
+
+char    *ft_strchr(const char *s, int c);
 
 #endif
