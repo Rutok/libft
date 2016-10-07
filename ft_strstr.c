fff4218d2e329eb981a827ebf3f353d54e7f85e3
Nicolas Boste <n.boste@gmail.com>
2016-10-07 18:26:02 +0200

    strstr

---
+++ ft_strstr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nboste <nboste@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/10/07 18:15:05 by nboste            #+#    #+#             */
+/*   Updated: 2016/10/07 18:24:31 by nboste           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_libft.h"
+
+char    *ft_strstr(const char *h, const char *n)
+{
+    char    *oc;
+    char    *t_n;
+
+    if (!*n)
+        return ((char *)h);
+    while (*h)
+    {
+        t_n = (char *)n;
+        while (*h && *t_n != *h)
+            h++;
+        oc = (char *)h;
+        while (*h && *t_n && *h == *t_n)
+        {
+            h++;
+            t_n++;
+        }
+        if (*t_n == '\0')
+            return (oc);
+        else
+            h = (const char *)(oc + 1);
+    }
+    return (NULL);
+}
