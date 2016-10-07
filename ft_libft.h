b884ea2d1449266258e8e0142366d3daeed88567
Nicolas Boste <n.boste@gmail.com>
2016-10-07 17:37:48 +0200

    strlcat

--- ft_libft.h
+++ ft_libft.h
@@ -6,7 +6,7 @@
 /*   By: nboste <nboste@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/06 21:21:09 by nboste            #+#    #+#             */
-/*   Updated: 2016/10/07 17:07:59 by nboste           ###   ########.fr       */
+/*   Updated: 2016/10/07 17:18:34 by nboste           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,5 +40,7 @@ char    *ft_strncpy(char *dest, const char *src, size_t n);
 char    *ft_strcat(char *dest, const char *src);
 
 char    *ft_strncat(char *dest, const char *src, size_t n);
+
+size_t  ft_strlcat(char *dest, const char *src, size_t size);
 
 #endif
---
+++ ft_strlcat.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nboste <nboste@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/10/07 17:19:56 by nboste            #+#    #+#             */
+/*   Updated: 2016/10/07 17:37:02 by nboste           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_libft.h"
+
+size_t  ft_strlcat(char *dest, const char *src, size_t size)
+{
+    size_t  i;
+
+    i = 0;
+    while (dest[i] && i < size)
+        i++;
+    if (i == (size - 1) && dest[i] != '\0')
+        return (size);
+    while (*src && i < (size - 1))
+        dest[i++] = *src++;
+    dest[i] = '\0';
+    return (i);
+}
