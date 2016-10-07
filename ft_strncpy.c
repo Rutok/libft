a1c2106bcfa934a7c59ed452775ba1176b9c55e3
Nicolas Boste <n.boste@gmail.com>
2016-10-07 19:52:03 +0200

    bad inc

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: nboste <nboste@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/07 16:52:39 by nboste            #+#    #+#             */
-/*   Updated: 2016/10/07 16:56:09 by nboste           ###   ########.fr       */
+/*   Updated: 2016/10/07 19:51:29 by nboste           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ char    *ft_strncpy(char *dest, const char *src, size_t n)
     size_t  i;
 
     i = 0;
-    while (i++ < n && *src)
-        dest[i] = *src++;
+    while (i < n && *src)
+        dest[i++] = *src++;
     while (i++ < n)
         dest[i] = '\0';
     return (dest);
